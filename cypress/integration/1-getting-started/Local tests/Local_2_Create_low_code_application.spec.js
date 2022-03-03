31a691981da4ed79eca4af9f9f82edef25c26190
Bojan Svirkov <bojan_svirkov@hotmail.com>
2022-03-03 17:04:23 +0100

    update

--- cypress/integration/1-getting-started/Local tests/Local_2_Create_low_code_application.spec.js
+++ cypress/integration/1-getting-started/Local tests/Local_2_Create_low_code_application.spec.js
@@ -9,23 +9,26 @@ it('should be able to visit the page', () => {
 
 it('should be able to enter login credentials', () => {
 
-    cy.get(':nth-child(1) > .form-control').type("bojan.svirkov@planetcrust.com") // Here in .type("Email") write your email address for your account
-    cy.get(':nth-child(2) > .form-control').type("Corteza123") // In .type("PASS") write the password for your account
+    cy.get('[name=email]').type("bojan.svirkov@planetcrust.com") // Here in .type("Email") write your email address for your account
+    cy.get('[name=password]').type("Corteza123") // In .type("PASS") write the password for your account
 
 })
 
 it('should be able to login in', () => {
 
     cy.get('.btn-primary').click()
+    cy.get('[href="/"] > .bi').click()
+    cy.get('.close').click()
+    cy.visit(baseURL + '/compose/namespaces')
 
 })
 
 
-    //cy.get('.btn-secondary').click()
-    //cy.visit('https://qc.cortezaproject.org/compose/')
-    //cy.get('.btn-primary').click()
-    //cy.get('#ns-nm').type("Cypress Test Namespace") // in .type("Name") write the name of your namespace
-    //cy.get('#__BVID__97').type("test_namespace") // in .type("handle") write the short name/handle of your namespace
-    //cy.get('.d-flex > .btn-light').click()
-    //cy.get('[href="/compose/ns/test_namespace/pages"]').click() // here in the link where it says test_namespace, use your handle that you gave in the function above
+it('should be able to create a new namespace', () => {
 
+    cy.get('.btn-primary').click()
+    cy.get('#ns-nm').type("Cypress test namespace")    // in .type("Name") write the name of your namespace
+    cy.get('[id=__BVID__114]').type("Cypress_test_NS")  // in .type("handle") write the short name/handle of your namespace
+    cy.get('.d-flex > .btn-light').click()
+
+})
