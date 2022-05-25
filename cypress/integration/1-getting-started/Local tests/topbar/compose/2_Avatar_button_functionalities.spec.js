10279d3101a57122e7db3005b709644d655529d5
Bojan Svirkov <bojan_svirkov@hotmail.com>
2022-05-25 16:49:06 +0200

    Added topbar tests for basic functionalities of all webapps

---
+++ cypress/integration/1-getting-started/Local tests/topbar/compose/2_Avatar_button_functionalities.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+const baseURL = Cypress.env("baseURL");
+// Before running this test first run the Server test 2 in order to create a user so you can log in.
+describe("Test avatar functionalities", () => {
+  context("Test for logging in the user", () => {
+    it("should be able to log in successfully", () => {
+      cy.visit(baseURL + "/"); // Localhost in the env file should be changed to reflect the Compose host. We are visiting the main authentication page here.
+      cy.get('[data-test-id="input-email"]').type("cypress@test.com"); // Here write your email address for your account
+      cy.get('[data-test-id="input-password"]').type("cypress123"); // Write the password for your account
+      cy.get('[data-test-id="button-login-and-remember"]').click(); // Here we click on the log in and remember me button
+      cy.visit(baseURL + "/compose/namespaces"); // Visiting main page of Compose
+    });
+  });
+
+  context("Test for checking which user is logged in", () => {
+    it("should be able to see which user is logged in", () => {
+      cy.get('[data-test-id="dropdown-profile"]').click(); // We click on the profile (avatar) icon and can see which user is logged in
+    });
+  });
+
+  context("Test for redirecting the user to the auth page", () => {
+    it("should be able to click on the profile button and be redirected to the auth page", () => {
+      cy.get('[data-test-id="dropdown-profile-user"]').click(); // We click on the profile button
+    });
+  });
+
+  context("Test for redirecting the user to change password auth page", () => {
+    it("should be able to click on the change password button and be redirected to the auth page", () => {
+      cy.get('[data-test-id="dropdown-profile-change-password"]').click({ force: true }); // We click on change password button
+    });
+  });
+
+  context("Test for logging out the user", () => {
+    it("should be able to log out", () => {
+      cy.get('[data-test-id="dropdown-profile-logout"]').click({ force: true }); // We click on the log out button
+    });
+  });
+});
---
+++ cypress/integration/1-getting-started/Local tests/topbar/workflows/1_Helper_dropdown_button_functionalities.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+const baseURL = Cypress.env("baseURL");
+// Before running this test first run the Server test 2 in order to create a user so you can log in.
+describe("Test helper dropdown functionalities", () => {
+  context("Test for logging in the user", () => {
+    it("should be able to log in successfully", () => {
+      cy.visit(baseURL + "/"); // Localhost in the env file should be changed to reflect the Workflows host. We are visiting the main authentication page here.
+      cy.get('[data-test-id="input-email"]').type("cypress@test.com"); // Here write your email address for your account
+      cy.get('[data-test-id="input-password"]').type("cypress123"); // Write the password for your account
+      cy.get('[data-test-id="button-login-and-remember"]').click(); // Here we click on the log in and remember me button
+      cy.visit(baseURL + "/list"); // Visiting main page of Workflows
+    });
+  });
+
+  context("Test for opening up the forum page", () => {
+    it("should be able to access the forum page", () => {
+      cy.get('[data-test-id="dropdown-helper-forum"]').click({ force: true }); // We access the forum
+    });
+  });
+
+  context("Test for opening up the documentation page", () => {
+    it("should be able to open corteza documentation", () => {
+      cy.get('[data-test-id="dropdown-helper-docs"]').click({ force: true }); // We open the docs through this funtion
+    });
+  });
+
+  context("Test for using the send feedback feature", () => {
+    it("should be able to use the send feedback feature", () => {
+      cy.wait(10000);
+      cy.get('[data-test-id="dropdown-helper-feedback"]').click({force: true}); // We use the send feedback feature with this function
+    });
+  });
+});
