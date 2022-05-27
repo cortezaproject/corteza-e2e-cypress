fe52b5348daed38693026b5fb8feb873bb69c536
Bojan Svirkov <bojan_svirkov@hotmail.com>
2022-05-27 15:44:11 +0200

    Tests formated and changed the name of the main folder

---
+++ cypress/integration/1-getting-started/local_tests/compose/Compose_2_Create_a_page_to_store_data.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+const baseURL = Cypress.env("baseURL");
+// Once your namespace is created from the previous step 'Local_2_Create_low_code_application', we are going to use the same namespace here
+
+it("Log in and access the already created NS", () => {
+  // First we will need to log in and access the already created NS from the previos test;
+  cy.visit(baseURL + "/compose/namespaces"); // When testing things locally compose part should be removed
+  cy.get('[data-test-id="input-email"]').type("EMAIL"); // Here in .type("Email") write your email address for your account
+  cy.get('[data-test-id="input-password"]').type("PASS"); // In .type("PASS") write the password for your account
+  cy.get('[data-test-id="button-login-and-remember"]').click();
+  cy.get('[href="/compose/ns/>>HANDLE<</pages"]').click(); // Here we use the handle (after /ns/) from the created NS (When testing things locally compose part should be removed)
+});
+
+it("Create a page (Module) to store data", () => {
+  cy.get('[data-test-id="button-admin"]').click(); // Accessing the admin panel
+  cy.get('[data-test-id="button-create"]').click(); // Clicking on New Module button
+  cy.get('[data-test-id="input-module-name"]').type("Cypress Test Module"); // Typing the Module name
+  cy.get('[data-test-id="input-module-handle"]').type("Cypress_Test_Module"); // Typing the Module Handle
+  cy.get("input:eq(4)").clear().type("Name"); // Entering the name of the first module record field
+  cy.get("input:eq(5)").clear().type("Name"); // Entering the title of the first module record field
+  cy.get('[data-test-id="button-field-add"]').click(); // Clicking on the + Add new field button
+  cy.get("input:eq(7)").type("Surname"); // Entering the name of the Second module record field
+  cy.get("input:eq(8)").type("Surname"); // Entering the title of the Second module record field
+  cy.get('[data-test-id="button-field-add"]').click(); // Clicking on the + Add new field button
+  cy.get("input:eq(10)").type("Age"); // Entering the name of the third module record field
+  cy.get("input:eq(11)").type("Age"); // Entering the title of the Second module record field
+  cy.get("select:eq(2)").select("Number input"); // Selecting the type of the record, in this case Number input
+  cy.get('[data-test-id="button-save-and-close"]').click(); // Clicking on Save and close
+});
---
+++ cypress/integration/1-getting-started/local_tests/server/Server_1_Create_a_user_with_misconfigured_email.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+const baseURL = Cypress.env("baseURL");
+
+it("should be able to visit the login page", () => {
+  cy.visit(baseURL + "/"); // When running this test make sure that the base url is set to localhost:3000
+});
+
+it("should be able to click on the create a new account button and go to the signup page", () => {
+  cy.get('[data-test-id="link-signup"]').click();
+});
+
+it("should be able to write signup credentials, create an account and log in", () => {
+  cy.get('[data-test-id="input-email"]').type("cypress_email"); // THIS WILL BE LEFT LIKE THIS SO IT'S MISCONFIGURED AND WE RECEIVE AN ERROR/WARNING;  On "EMAIL" we type the email of the account that we want to create
+  cy.get('[data-test-id="input-password"]').type("password"); // On "Password" we type the password that we want to use
+  cy.get('[data-test-id="input-name"]').type("cypress account"); // We type the name of the account/user here
+  cy.get('[data-test-id="input-handle"]').type("cypress_handle"); // We type the handle for the account
+  cy.get('[data-test-id="button-submit"]').click(); // We click on the submit button
+});
