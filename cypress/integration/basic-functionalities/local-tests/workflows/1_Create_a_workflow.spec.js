8c556fa843b35301b721725acd7b60bca957627a
Bojan Svirkov <bojan_svirkov@hotmail.com>
2022-06-09 15:03:08 +0200

    Upgraded first layer workflow tests

--- cypress/integration/basic-functionalities/local-tests/workflows/1_Create_a_workflow.spec.js
+++ cypress/integration/basic-functionalities/local-tests/workflows/1_Create_a_workflow.spec.js
@@ -3,14 +3,56 @@ const baseURL = Cypress.env('baseURL')
 const email = Cypress.env('user').email
 const password = Cypress.env('user').password
 
-// Before running this test first run the Server test 2 in order to create a user so you can log in.
-describe('Test for creating a simple workflow', () => {
+// Before running this test first run the Server signup test 2 in order to create a user so you can log in.
+describe('Test for creating a simple workflow and checking its functionalities', () => {
   before(() => {
     cy.login({ email, password })
   })
 
+  context('Test for creating a workflow with no info entered', () => {
+    it('should not be able to create a workflow', () => {
+      cy.get('[data-test-id="button-create-workflow"]').click()
+      cy.get('[data-test-id="input-label"]').clear()
+      cy.get('[data-test-id="button-save-workflow"]').click()
+      cy.get('.b-toast-warning') // We check if the warning toast appears
+    })
+  })
+
+  context('Test for creating a workflow with missing handle', () => {
+    it('should not be able to create a workflow with missing handle', () => {
+      cy.visit(baseURL + '/list')
+      cy.get('[data-test-id="button-create-workflow"]').click()
+      cy.get('[data-test-id="button-save-workflow"]').click()
+      cy.get('.b-toast-warning') // We check if the warning toast appears
+    })
+  })
+
+  context('Test for creating a workflow with invalid handle', () => {
+    it('should not be able to create a workflow with invalid handle', () => {
+      cy.visit(baseURL + '/list')
+      cy.get('[data-test-id="button-create-workflow"]').click()
+      cy.get('[data-test-id="input-label"]').clear().type('Cypress workflow')
+      cy.get('[data-test-id="input-handle"]').type('_')
+      cy.get('[data-test-id="input-description"]').type('This is a simple workflow created by an automated cypress test.')
+      cy.get('[data-test-id="button-save-workflow"]').click()
+      cy.get('[data-test-id="input-handle-invalid-state"]')
+      cy.get('.b-toast-warning') // We check if the warning toast appears
+    })
+  })
+
+  context('Test for checking if export, import and permissions buttons are present when creating a workflow', () => {
+    it('should not be able to see the buttons present', () => {
+      cy.visit(baseURL + '/list')
+      cy.get('[data-test-id="button-create-workflow"]').click()
+      cy.get('[data-test-id="button-import-workflow"]').should('not.exist')
+      cy.get('[data-test-id="button-export-workflow"]').should('not.exist')
+      cy.get('[data-test-id="button-permissions"]').should('not.exist')
+    })
+  })
+
   context('Test for creating a new workflow', () => {
     it('should be able to create a new workflow', () => {
+      cy.visit(baseURL + '/list')
       cy.get('[data-test-id="button-create-workflow"]').click()
       cy.get('[data-test-id="input-label"]').clear().type('Cypress workflow')
       cy.get('[data-test-id="input-handle"]').type('cypress_workflow')
--- cypress/integration/basic-functionalities/local-tests/workflows/2_Edit_the_created_workflow_from_test_1.spec.js
+++ cypress/integration/basic-functionalities/local-tests/workflows/2_Edit_the_created_workflow_from_test_1.spec.js
@@ -3,22 +3,35 @@ const baseURL = Cypress.env('baseURL')
 const email = Cypress.env('user').email
 const password = Cypress.env('user').password
 
-// Before running this test first run the Server test 2 in order to create a user so you can log in.
+// Before running this test first run the Server signup test 2 in order to create a user so you can log in.
 describe('Test for editing a workflow', () => {
   before(() => {
     cy.login({ email, password })
   })
+  context('Test for checking if export, import and permissions buttons are present when editing a workflow', () => {
+    it('should be able to see the buttons', () => {
+      cy.contains('Cypress workflow').click()
+      cy.get('[data-test-id="button-configure-workflow"]').click()
+      cy.get('[data-test-id="button-import-workflow"]')
+      cy.get('[data-test-id="button-export-workflow"]')
+      cy.get('[data-test-id="button-permissions"]')
+    })
+  })
 
   context('Test for editing a workflow', () => {
     it('should be able to edit the workflow', () => {
-      cy.contains('Cypress workflow').click()
-      cy.get('[data-test-id="button-configure-workflow"]').click()
       cy.get('[data-test-id="input-label"]').type(' edited')
       cy.get('[data-test-id="input-handle"]').type('_edited')
-      cy.get('[data-test-id="input-description"]').type(' And this description just got edited.')
+      cy.get('[data-test-id="input-description"]').clear().type('Edited description.')
       cy.get('[data-test-id="button-save-workflow"]').click()
       cy.get('.b-toast-success') // We confirm that the action was completed successfully
       cy.get('.close').click({ multiple: true })
+
+      // We check below if the fields were really edited/changed
+      cy.get('[data-test-id="button-configure-workflow"]').click()
+      cy.get('[data-test-id="input-label"]').should('have.value', 'Cypress workflow edited')
+      cy.get('[data-test-id="input-handle"]').should('have.value', 'cypress_workflow_edited')
+      cy.get('[data-test-id="input-description"]').should('have.value', 'Edited description.')
     })
   })
 })
