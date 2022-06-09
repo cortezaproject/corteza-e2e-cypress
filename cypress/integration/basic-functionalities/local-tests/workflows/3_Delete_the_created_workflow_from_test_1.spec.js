0ee19c6306d2f5d1d2373a5d263562666c28377d
Bojan Svirkov <bojan_svirkov@hotmail.com>
2022-06-09 15:03:08 +0200

    Upgraded first layer workflow tests

--- cypress/integration/basic-functionalities/local-tests/workflows/3_Delete_the_created_workflow_from_test_1.spec.js
+++ cypress/integration/basic-functionalities/local-tests/workflows/3_Delete_the_created_workflow_from_test_1.spec.js
@@ -3,7 +3,7 @@ const baseURL = Cypress.env('baseURL')
 const email = Cypress.env('user').email
 const password = Cypress.env('user').password
 
-// Before running this test first run the Server test 2 in order to create a user so you can log in.
+// Before running this test first run the Server signup test 2 in order to create a user so you can log in.
 describe('Test for deleting a workflow', () => {
   before(() => {
     cy.login({ email, password })
@@ -16,6 +16,7 @@ describe('Test for deleting a workflow', () => {
       cy.get('[data-test-id="button-delete"]').click()
       cy.get('[data-test-id="button-delete-confirm"]').click()
       cy.get('.b-toast-success') // We confirm that the action was completed successfully
+      cy.contains('Cypress workflow').should('not.exist')
     })
   })
 })
