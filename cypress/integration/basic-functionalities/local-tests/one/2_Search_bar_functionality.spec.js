b193addb8009f25ce9ec0e4dcb19912f551c4f77
Bojan Svirkov <bojan_svirkov@hotmail.com>
2022-07-26 17:03:50 +0200

    Modified webapp one search bar tests

--- cypress/integration/basic-functionalities/local-tests/one/2_Search_bar_functionality.spec.js
+++ cypress/integration/basic-functionalities/local-tests/one/2_Search_bar_functionality.spec.js
@@ -3,65 +3,34 @@ const baseURL = Cypress.env('baseURL')
 const email = Cypress.env('user').email
 const password = Cypress.env('user').password
 
-// Before running this test first run the Server test 2 in order to create a user so you can log in.
-describe('Test search bar functionality', () => {
+// Before running this test first sign up
+describe('Test for creating a role', () => {
   before(() => {
     cy.login({ email, password })
   })
 
-  context('Test for searching the Admin Area', () => {
+  context('Test for searching a non existing app', () => {
+    it('should not be able to search for a non existing app', () => {
+      // Here we close the start tour pop up
+      cy.get('.modal-header > :last-child()').click()
+      cy.get('[data-test-id="input-search"]').type('xw')
+      cy.get('[data-test-id="heading-no-apps"]').should('exist')
+    })
+  })
+
+  context('Test for searching an existing app', () => {
     it('should be able to search for the Admin Area', () => {
-      cy.get('.modal-header > :last-child()').click() // Here we close the start tour pop up
-      cy.get('[data-test-id="input-search"]').type('admin')
+      cy.get('[data-test-id="input-search"]').clear().type('admin')
       cy.get('[data-test-id="Admin Area"]').click({ force: true })
+      cy.url().should('exist', baseURL + '/admin')
     })
-  })
 
-  context('Test for searching the CRM', () => {
-    it('should be able to search for the CRM', () => {
-      cy.get('.modal-header > :last-child()').click() // Here we close the start tour pop up
-      cy.get('[data-test-id="input-search"]').type('CRM')
-      cy.get('[data-test-id="CRM Suite"]').click({ force: true })
-    })
-  })
-
-  context('Test for searching the Low Code', () => {
-    it('should be able to search for the Low Code', () => {
-      cy.get('.modal-header > :last-child()').click() // Here we close the start tour pop up
+    it('should be able to search for the Low Code (Compose)', () => {
+      // Here we close the start tour pop up
+      cy.get('.modal-header > :last-child()').click()
       cy.get('[data-test-id="input-search"]').clear().type('low code')
       cy.get('[data-test-id="Low Code"]').click({ force: true })
-    })
-  })
-
-  context('Test for searching the Workflows', () => {
-    it('should be able to search for the Workflows', () => {
-      cy.get('.modal-header > :last-child()').click() // Here we close the start tour pop up
-      cy.get('[data-test-id="input-search"]').clear().type('workflow')
-      cy.get('[data-test-id="Workflows"]').click({ force: true })
-    })
-  })
-
-  context('Test for searching the Reporter', () => {
-    it('should be able to search for the Reporter', () => {
-      cy.get('.modal-header > :last-child()').click() // Here we close the start tour pop up
-      cy.get('[data-test-id="input-search"]').clear().type('reporter')
-      cy.get('[data-test-id="Reporter"]').click({ force: true })
-    })
-  })
-
-  context('Test for searching the Case Management', () => {
-    it('should be able to search for the Case Management', () => {
-      cy.get('.modal-header > :last-child()').click() // Here we close the start tour pop up
-      cy.get('[data-test-id="input-search"]').clear().type('case management')
-      cy.get('[data-test-id="Case Management"]').click({ force: true })
-    })
-  })
-
-  context('Test for searching the Jitsi Bridge', () => {
-    it('should be able to search for the Jitsi Bridge', () => {
-      cy.get('.modal-header > :last-child()').click() // Here we close the start tour pop up
-      cy.get('[data-test-id="input-search"]').clear().type('jitsi')
-      cy.get('[data-test-id="Jitsi Bridge"]').click({ force: true })
+      cy.url().should('exist', baseURL + '/compose')
     })
   })
 })
