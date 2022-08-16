cb28eceb0e4f4bf1085107efbdf6e6150133af6b
Bojan Svirkov <bojan_svirkov@hotmail.com>
2022-08-16 18:10:22 +0200

    Added reporter delete test for removing the remaining resources

--- cypress/integration/basic-functionalities/local-tests/reporter/Delete_report.spec.js
+++ cypress/integration/basic-functionalities/local-tests/reporter/Delete_report.spec.js
@@ -19,6 +19,14 @@ describe('Test for deleting a report', () => {
       // We check if the success toast appears
       cy.get('.b-toast-success') 
       cy.contains('cypress_handle').should('not.exist')
+
+      // We click on the Edit button on the created report
+      cy.get('table > tbody > :first-child() > :last-child() > a:nth-child(2)').click() 
+      cy.get('[data-test-id="button-delete"]').click()
+      cy.get('[data-test-id="button-delete-confirm"]').click()
+      // We check if the success toast appears
+      cy.get('.b-toast-success') 
+      cy.contains('another_cypress_report').should('not.exist')
     })
   })
 })
