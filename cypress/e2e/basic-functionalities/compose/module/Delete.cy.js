63f82601d997898a2ea6b1e2770372dc9d9bb58e
Katrin Yordanova <katrin.yordanova@crust.tech>
2024-01-11 16:10:10 +0200

    Fix compose ns and module tests;gp origin 2023.9.x-provisionin-basic-tests

--- cypress/e2e/basic-functionalities/compose/module/Delete.cy.js
+++ cypress/e2e/basic-functionalities/compose/module/Delete.cy.js
@@ -1,25 +1,37 @@
 /// <reference types="cypress" />
+import {
+  provisionAll,
+  provisionDefaultNamespaceCreate,
+  provisionDefaultModuleCreate
+} from '../../../../provision/list'
+
 const composeURL = Cypress.env('COMPOSE_URL')
-const email = Cypress.env('USER_EMAIL')
-const password = Cypress.env('USER_PASSWORD')
 
 describe('Test for deleting a module', () => {
   before(() => {
-    if (!window.sessionStorage.getItem('auth.refresh-token')) {
-      cy.login({ email, password, url: composeURL })
-    }
+    cy.seedDb([
+      ...provisionAll,
+      ...provisionDefaultNamespaceCreate,
+      ...provisionDefaultModuleCreate
+    ])
+
+    cy.preTestLogin({ url: composeURL })
   })
 
   context('Test for deleting a module', () => {
     it('should be able to delete the module', () => {
+      cy.intercept('/api/compose/namespace/37169336485952369/module/?query=cypress&limit=100&incTotal=true&pageCursor=&sort=name+ASC')
+      .as('module-search')
       cy.visit(composeURL + '/namespaces')
       cy.get('[data-test-id="input-search"]').type('cypress')
       cy.get('[data-test-id="link-visit-namespace-cypress_namespace"]').click({ force: true })
-      cy.get('[data-test-id="button-admin"]', { timeout: 10000 }).click()
-      cy.wait(1000)
-      cy.get('[data-test-id="table-modules-list"] > .card-body > div > #resource-list > tbody').click()
+      cy.get('[data-test-id="button-admin"]', { timeout: 10000 }).click({ force: true })
+      cy.get('[data-test-id="table-modules-list"] [data-test-id="input-search"]')
+        .type('cypress')
+      cy.wait('@module-search')
+      cy.get('#resource-list td:nth-child(2)', { timeout: 10000 }).click({ force: true })
       cy.get('[data-test-id="editor-toolbar"]', { timeout: 10000 }).within(() => {
-        cy.get('[data-test-id="button-delete"]').click()
+        cy.get('[data-test-id="button-delete"]').click({ force: true })
         cy.get('[data-test-id="button-delete-confirm"]').click({ force: true })
       })
       cy.contains('cypress_module').should('not.exist')
--- cypress/e2e/basic-functionalities/compose/namespace/Clone.cy.js
+++ cypress/e2e/basic-functionalities/compose/namespace/Clone.cy.js
@@ -1,25 +1,30 @@
 /// <reference types="cypress" />
+import { provisionAll, provisionDefaultNamespaceCreate } from '../../../../provision/list'
+
 const composeURL = Cypress.env('COMPOSE_URL')
-const email = Cypress.env('USER_EMAIL')
-const password = Cypress.env('USER_PASSWORD')
 
 describe('Test for editing a record', () => {
   before(() => {
-    if (!window.sessionStorage.getItem('auth.refresh-token')) {
-      cy.login({ email, password, url: composeURL })
-    }
+    cy.seedDb([...provisionAll, ...provisionDefaultNamespaceCreate])
+    cy.preTestLogin({ url: composeURL })
+    cy.visit(composeURL + '/namespaces')
   })
   
   context('Test for cloning a namespace', () => {
     it('should be able to clone a namespace', () => {
-      cy.visit(composeURL + '/namespaces')
-      cy.get('[data-test-id="button-manage-namespaces"]').click()
-      cy.get('[data-test-id="input-search"]').type('edited')
-      // We wait 1s in order the page to be loaded
-      cy.wait(1000)
-      cy.get('tbody').click()
-      cy.get('[data-test-id="button-clone"]').click()
-      cy.url().should('exist', '/clone/')
+      cy.intercept('/api/compose/namespace/?query=&limit=100&incTotal=true&sort=name+ASC')
+        .as('ns-manage')
+      cy.intercept('/api/compose/namespace/?query=cypress&limit=100&incTotal=true&pageCursor=&sort=name+ASC')
+        .as('ns-search')
+      cy.get('[data-test-id="button-manage-namespaces"]').click({ force: true })
+      cy.wait('@ns-manage')
+      cy.get('[data-test-id="input-search"]').type('cypress')
+      cy.wait('@ns-search')
+      cy.get('tbody').contains('cypress').should('exist').click({ force: true })
+      cy.get('[data-test-id="button-clone"]').click({ force: true })
+      cy.url().should('contain', '/clone/')
+      cy.get('[data-test-id="button-save"]').click({ force: true })
+      cy.url().should('contain', '/edit')
     })
   })
 })
