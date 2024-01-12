6c5058eccbed56330b979ca0032413e4beae3774
Katrin Yordanova <katrin.yordanova@crust.tech>
2024-01-12 13:23:06 +0200

    Fix compose record-page tests

--- cypress/e2e/basic-functionalities/compose/record-page/Create.cy.js
+++ cypress/e2e/basic-functionalities/compose/record-page/Create.cy.js
@@ -1,41 +1,53 @@
 /// <reference types="cypress" />
+import {
+  provisionAll,
+  provisionDefaultNamespaceCreate,
+  provisionDefaultModuleCreate,
+  provisionDefaultFieldCreate,
+} from '../../../../provision/list'
+
 const composeURL = Cypress.env('COMPOSE_URL')
-const email = Cypress.env('USER_EMAIL')
-const password = Cypress.env('USER_PASSWORD')
 
 describe('Test for creating a record page', () => {
   before(() => {
-    if (!window.sessionStorage.getItem('auth.refresh-token')) {
-      cy.login({ email, password, url: composeURL })
-    }
+    cy.seedDb([
+      ...provisionAll,
+      ...provisionDefaultNamespaceCreate,
+      ...provisionDefaultModuleCreate,
+      ...provisionDefaultFieldCreate,
+    ])
+  })
+
+  beforeEach(() => {
+    cy.preTestLogin({ url: composeURL })
+
+    cy.visit(composeURL + '/namespaces')
+    cy.get('[data-test-id="input-search"]').type('cypress')
+    cy.get('[data-test-id="link-visit-namespace-cypress_namespace"]').click({ force: true })
+    cy.get('[data-test-id="button-admin"]').click({ force: true })
   })
 
   context('Test for creating a record page', () => {
     it('should be able to create a record page', () => {
-      cy.visit(composeURL + '/namespaces')
-      cy.get('[data-test-id="input-search"]').type('cypress')
-      cy.get('[data-test-id="link-visit-namespace-cypress_namespace"]').click({ force: true })
-      cy.contains('Welcome').should('exist')
-      cy.get('[data-test-id="button-admin"]').click()
-      cy.get('[data-test-id="button-record-page-create"]').click()
-      cy.get('.related-pages-dropdown').click()
+      cy.get('[data-test-id="table-modules-list"] [data-test-id="input-search"]')
+        .type('cypress')
+      cy.get('[data-test-id="button-record-page-create"]').click({ force: true })
+      cy.get('.related-pages-dropdown').click({ force: true })
       cy.get('[data-test-id="dropdown-link-record-list-page-create"]').click({ force: true })
-      cy.get('.nav-sidebar').contains('Pages').click()
-      cy.get('[data-test-id="button-page-view"]:first').click()
+      cy.visit(composeURL + '/ns/cypress_namespace/admin/pages')
+      cy.get('[data-test-id="button-page-view"]:first').click({ force: true })
     })
   })
 
   context('Test for creating a page', () => {
     it('should be able to create a page', () => {
-      cy.get('[data-test-id="button-admin"]').click()
-      cy.get('.nav-sidebar').contains('Pages').click()
+      cy.get('.nav-sidebar').contains('Pages').click({ force: true })
       cy.get('[data-test-id="input-name"]').type('New Page')
-      cy.get('[data-test-id="button-create-page"]').click()
+      cy.get('[data-test-id="button-create-page"]').click({ force: true })
       cy.get('[data-test-id="input-handle"]').type('new_cypress_page')
       cy.get('[data-test-id="input-description"]').type('Description')
-      // Toggling page visibility
-      cy.get('[data-test-id="checkbox-page-visibility"]').check({force: true}) 
-      cy.get('[data-test-id="button-save-and-close"]').click()
+      cy.get('[data-test-id="checkbox-page-visibility"]').check({ force: true }) 
+      cy.get('[data-test-id="button-save-and-close"]').click({ force: true })
     })
   })
 })
---
+++ cypress/provision/page_delete.sql
@@ -0,0 +1,3 @@
+delete from compose_page where title = 'Record List for module "Cypress module"';
+delete from compose_page where title = 'Record page for module "Cypress module"';
+delete from compose_page where handle = 'new_cypress_page';
