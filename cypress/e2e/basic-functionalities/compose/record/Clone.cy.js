95aa34b75da884ab4b9449c145c60fd7d84b381a
Katrin Yordanova <katrin.yordanova@crust.tech>
2024-01-19 18:05:54 +0200

    Fix record create and cone tests

--- cypress/e2e/basic-functionalities/compose/record/Clone.cy.js
+++ cypress/e2e/basic-functionalities/compose/record/Clone.cy.js
@@ -1,34 +1,51 @@
 /// <reference types="cypress" />
+import {
+  provisionAll,
+  provisionDefaultNamespaceCreate,
+  provisionDefaultModuleCreate,
+  provisionDefaultFieldCreate,
+  provisionDefaultPageCreate,
+  provisionDefaultRecordCreate,
+} from '../../../../provision/list'
+
 const composeURL = Cypress.env('COMPOSE_URL')
-const email = Cypress.env('USER_EMAIL')
-const password = Cypress.env('USER_PASSWORD')
 
 describe('Test for editing a record', () => {
   before(() => {
-    if (!window.sessionStorage.getItem('auth.refresh-token')) {
-      cy.login({ email, password, url: composeURL })
-    }
+    cy.seedDb([
+      ...provisionAll,
+      ...provisionDefaultNamespaceCreate,
+      ...provisionDefaultModuleCreate,
+      ...provisionDefaultFieldCreate,
+      ...provisionDefaultPageCreate,
+      ...provisionDefaultRecordCreate,
+    ])
+
+    cy.preTestLogin({ url: composeURL })
   })
   
   context('Test for cloning a record', () => {
-    it('should be able to clone a record ', () => {
-      cy.get('.nav-sidebar').contains('Modules').click()
-      // We wait 1s in order the page to be fully loaded
-      cy.wait(1000)
-      cy.get('[data-test-id="button-back-without-save"]', { timeout: 10000 }).click()
-      cy.get('[data-test-id="button-all-records"]').click()
-      cy.get('table > tbody').find('tr:first').within(() => {
-        cy.get('td').find('a:eq(1)').click()
-      })
+    it('should be able to clone a record and check if the changes were persisted', () => {
+      cy.visit(composeURL + '/namespaces')
+      cy.get('[data-test-id="input-search"]').type('cypress')
+      cy.get('[data-test-id="link-visit-namespace-cypress_namespace"]').click({ force: true })
+      cy.get('[data-test-id="button-admin"]', { timeout: 10000 })
+        .should('exist')
+        .click({ force: true })
+      cy.get('#resource-list td:nth-child(2)', { timeout: 10000 }).click({ force: true })  
+      cy.get('[data-test-id="button-all-records"] a').click({ force: true })
+      cy.get('.record-list-table td:nth-child(2)', { timeout: 10000 })
+        .eq(0)
+        .click({ force: true })
+      cy.get('[data-test-id="button-edit"]').click({ force: true })
+      cy.get('[data-test-id="button-clone"]').click({ force: true })
+      cy.get('[data-test-id="field-name"] input').clear().type('Bob')
+      cy.get('[data-test-id="field-surname"] input').clear().type('Wiser')
+      cy.get('[data-test-id="field-age"] input').clear().type(31)
+      cy.get('[data-test-id="button-save"]').click({ force: true })
 
-      cy.get('[data-test-id="button-clone"]').click()
-      cy.get('input:nth-child(1)').eq(1).clear().type('31')
-      cy.get('input:nth-child(1)').eq(2).clear().type('Bob')
-      cy.get('input:nth-child(1)').eq(3).clear().type('Wiser')
-      cy.get('[data-test-id="button-save"]').click()
-      // We wait 1s in order the page to be fully loaded
-      cy.wait(1000)
-      cy.get('.card-body', { timeout: 10000 }).contains('31').should('exist')
+      // Check if the changes were persisted
+      cy.get('.card-body').contains(31).should('exist')
       cy.get('.card-body').contains('Bob').should('exist')
       cy.get('.card-body').contains('Wiser').should('exist')
       cy.url().should('contain', '/record/')
