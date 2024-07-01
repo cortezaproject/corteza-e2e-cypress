bdfabdc1e66788b926f552b316fb951e2fb147a1
Peter Grlica <peter.grlica@gmail.com>
2024-07-01 13:27:02 +0200

    Refactored record tests to include API provisioning

--- cypress/e2e/basic-functionalities/compose/record/Clone.cy.js
+++ cypress/e2e/basic-functionalities/compose/record/Clone.cy.js
@@ -2,41 +2,56 @@
 import {
   provisionAll,
   provisionDefaultNamespaceCreate,
-  provisionDefaultModuleCreate,
-  provisionDefaultFieldCreate,
-  provisionDefaultPageCreate,
   provisionDefaultRecordCreate,
+  provisionDefaultRecordDelete,
+  provisionDefaultAuthClientCreate
 } from '../../../../provision/list'
+
+import { module } from '../../../../provision/api/compose'
 
 const composeURL = Cypress.env('COMPOSE_URL')
 
-describe('Test for editing a record', () => {
+describe('Test for cloning a record', () => {
   before(() => {
+    let jwtToken = ""
+
     cy.seedDb([
       ...provisionAll,
       ...provisionDefaultNamespaceCreate,
-      ...provisionDefaultModuleCreate,
-      ...provisionDefaultFieldCreate,
-      ...provisionDefaultPageCreate,
+      ...provisionDefaultAuthClientCreate,
+    ])
+
+    cy.authenticateClient()
+    .then(jt => jwtToken = jt)
+
+      // create module
+      .then(jwtToken => cy.moduleCreate(jwtToken, module))
+  })
+
+  beforeEach(() => {
+    cy.seedDb([
+      ...provisionDefaultRecordDelete,
       ...provisionDefaultRecordCreate,
     ])
 
     cy.preTestLogin({ url: composeURL })
+
+    // we got this via provisioning
+    cy.visit(composeURL, { retryOnStatusCodeFailure: true, timeout: 30000 })
+    cy.visit(composeURL + '/ns/cypress_namespace/pages', { retryOnStatusCodeFailure: true, timeout: 30000 })
+    cy.get('[data-test-id="button-admin"]', { timeout: 10000 })
+      .should('exist')
+      .click({ force: true })
+    cy.get('#resource-list td:nth-child(2)', { timeout: 10000 }).click({ force: true })
+    cy.get('[data-test-id="button-all-records"] a', { timeout: 1000 }).click({ force: true })
   })
-  
+
   context('Test for cloning a record', () => {
     it('should be able to clone a record and check if the changes were persisted', () => {
-      cy.visit(composeURL + '/namespaces')
-      cy.searchItem()
-      cy.get('[data-test-id="link-visit-namespace-cypress_namespace"]').click({ force: true })
-      cy.get('[data-test-id="button-admin"]', { timeout: 10000 })
-        .should('exist')
-        .click({ force: true })
-      cy.get('#resource-list td:nth-child(2)', { timeout: 10000 }).click({ force: true })  
-      cy.get('[data-test-id="button-all-records"] a').click({ force: true })
       cy.get('.record-list-table td:nth-child(2)', { timeout: 10000 })
         .eq(0)
         .click({ force: true })
+
       cy.get('[data-test-id="button-edit"]').click({ force: true })
       cy.get('[data-test-id="button-clone"]').click({ force: true })
       cy.get('[data-test-id="field-name"] input').clear().type('Bob')
