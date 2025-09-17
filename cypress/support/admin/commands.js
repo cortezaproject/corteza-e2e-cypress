fce236c65857d49e1e2aa5523fe62e4ddf5e344e
Jože Fortun <joze.fortun@gmail.com>
2025-09-17 15:34:28 +0200

    Bump version and fix list search to be submittable in tests

--- cypress/support/admin/commands.js
+++ cypress/support/admin/commands.js
@@ -2,10 +2,24 @@ Cypress.Commands.add('navigateAdmin', ({ el = '.nav-sidebar', timeout = 10000, a
     cy.get(el, { timeout }).contains(app).click({ force: true })
 })
 
-Cypress.Commands.add('searchItem', ({ item_cy_id = 'input-search', item = 'cypress', timeout = 10000 } = {}) => {
+Cypress.Commands.add('searchItem', ({ 
+    item_cy_id = 'input-search', 
+    item = 'cypress', 
+    timeout = 10000, 
+    clickSearch = true,
+    searchButtonSelector = '.search-button'
+} = {}) => {
     cy.get(`[data-test-id="${item_cy_id}"]`, { timeout })
         .should('exist')
         .should('not.be', 'disabled')
         .clear()
         .type(item)
+    
+    // If clickSearch is true, click the search button
+    if (clickSearch) {
+        cy.get(searchButtonSelector, { timeout })
+            .should('exist')
+            .should('not.be.disabled')
+            .click()
+    }
 })
