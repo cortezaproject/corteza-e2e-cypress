583df7ca03413c8520974c31b7bd0936c3fdb877
Tomaž Jerman <tomaz.jerman121@gmail.com>
2025-11-05 14:49:23 +0100

    Fix provision after user group modifications

--- cypress/provision/auth_client_delete.sql
+++ cypress/provision/auth_client_delete.sql
@@ -1,3 +1,3 @@
 delete from users where id = 368979993402802178;
-delete from role_members where rel_user = 368979993402802178;
+delete from role_members where rel_resource = 'corteza::system:user/368979993402802178';
 delete from auth_clients where id = 374948212714242049;
--- cypress/provision/user_missing_delete.sql
+++ cypress/provision/user_missing_delete.sql
@@ -1,3 +1,3 @@
 delete from credentials where rel_owner = (select id from users where email = 'missing@email.com');
-delete from role_members where rel_user = (select id from users where email = 'missing@email.com');
+delete from role_members where rel_resource = (select concat('corteza::system:user/', id) from users where email = 'missing@email.com');
 delete from users where email = 'missing@email.com';
